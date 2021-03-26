9317cf5fa10d866586467daea23f95ff3314370b
Eldrick Millares <eldrick@stanford.edu>
2021-03-26 13:35:46 -0700

    Adds QED modules to design compilation

--- steelcore_demo/patches/patch.sh
+++ steelcore_demo/patches/patch.sh
@@ -2,6 +2,10 @@
 cp ./steelcore/rtl/*.v design/
 cp ./steelcore/rtl/*.vh design/
 
-# Write design file list
+# Write design files to design file list
 ls ./design/ | sed 's/^/.\/design\//; s/$//' > ./design/design.flist
 sed -i '/.\/design\/design.flist/d' ./design/design.flist
+
+# Write qed files to design file list
+echo "" >> ./design/design.flist
+ls ./qed/ | sed 's/^/.\/qed\//; s/$//' >> ./design/design.flist
