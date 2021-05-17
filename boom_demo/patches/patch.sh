14b24b748bd36a678e5553b4853426ef00de433c
Eldrick Millares <eldrick@stanford.edu>
2021-05-16 19:34:32 -0700

    Adds BOOM demo template files

---
+++ boom_demo/patches/patch.sh
@@ -0,0 +1,13 @@
+# Copy relevant source files to the design folder
+e.g. cp ./boom/*.v design/
+
+# Patch design files
+e.g. cp ./patches/wire_up/design_top.v design/
+
+# Write design files to design file list
+e.g. ls ./design/ | sed 's/^/.\/design\//; s/$//' > ./design/design.flist
+e.g. sed -i '/.\/design\/design.flist/d' ./design/design.flist
+
+# Write qed files to design file list
+e.g. echo "" >> ./design/design.flist
+e.g. ls ./qed/ | sed 's/^/.\/qed\//; s/$//' >> ./design/design.flist
