5c9261bc1b41a1d6f0a50f5a9b51f4c90f4bfbcf
Eldrick Millares <eldrick@stanford.edu>
2021-09-27 00:15:07 -0700

    Adds all stores and loads, adds branch instructions

--- steelcore_demo/qed/inst_constraints.sv
+++ steelcore_demo/qed/inst_constraints.sv
@@ -1,5 +1,5 @@
 // Copyright (c) Stanford University
-// 
+//
 // This source code is patent protected and being made available under the
 // terms explained in the ../LICENSE-Academic and ../LICENSE-GOV files.
 //
@@ -14,21 +14,33 @@ clk);
   input [31:0] instruction;
   input clk;
 
-  wire [4:0] shamt;
   wire [11:0] imm12;
+  wire [3:0] bimm4;
+  wire [9:0] jimm10;
+  wire bimm11;
+  wire [19:0] uimm31;
+  wire jimm11;
   wire [4:0] rd;
+  wire jimm20;
   wire [2:0] funct3;
   wire [6:0] opcode;
   wire [6:0] imm7;
+  wire bimm12;
   wire [6:0] funct7;
+  wire [5:0] bimm10;
   wire [4:0] imm5;
   wire [4:0] rs1;
   wire [4:0] rs2;
-  wire [9:0] jimm10;
-  wire jimm11;
   wire [7:0] jimm19;
-  wire jimm20;
-  wire [19:0] uimm31;
+
+  wire FORMAT_B;
+  wire ALLOWED_B;
+  wire BGEU;
+  wire BGE;
+  wire BLT;
+  wire BLTU;
+  wire BEQ;
+  wire BNE;
 
   wire FORMAT_I;
   wire ALLOWED_I;
@@ -46,9 +58,19 @@ clk);
   wire ALLOWED_J;
   wire JAL;
 
+  wire FORMAT_SW;
+  wire ALLOWED_SW;
+  wire SB;
+  wire SH;
+  wire SW;
+
   wire FORMAT_LW;
   wire ALLOWED_LW;
+  wire LBU;
+  wire LH;
   wire LW;
+  wire LHU;
+  wire LB;
 
   wire FORMAT_R;
   wire ALLOWED_R;
@@ -63,11 +85,6 @@ clk);
   wire ADD;
   wire OR;
 
-  wire FORMAT_SW;
-  wire ALLOWED_SW;
-  wire SW;
-
-
   wire FORMAT_AUIPC;
   wire ALLOWED_AUIPC;
   wire AUIPC;
@@ -79,21 +96,33 @@ clk);
   wire ALLOWED_NOP;
   wire NOP;
 
-  assign shamt = instruction[24:20];
   assign imm12 = instruction[31:20];
+  assign bimm4 = instruction[11:8];
+  assign jimm10 = instruction[30:21];
+  assign bimm11 = instruction[7:7];
+  assign uimm31 = instruction[31:12];
+  assign jimm11 = instruction[20:20];
   assign rd = instruction[11:7];
+  assign jimm20 = instruction[31:31];
   assign funct3 = instruction[14:12];
   assign opcode = instruction[6:0];
   assign imm7 = instruction[31:25];
+  assign bimm12 = instruction[31:31];
   assign funct7 = instruction[31:25];
+  assign bimm10 = instruction[30:25];
   assign imm5 = instruction[11:7];
   assign rs1 = instruction[19:15];
   assign rs2 = instruction[24:20];
-  assign jimm10 = instruction[30:21];
-  assign jimm11 = instruction[20:20];
   assign jimm19 = instruction[19:12];
-  assign jimm20 = instruction[31:31];
-  assign uimm31 = instruction[31:12];
+
+  assign FORMAT_B = (rs2 < 16) && (rs1 < 16);
+  assign BGEU = FORMAT_B && (funct3 == 3'b111) && (opcode == 7'b1100011);
+  assign BGE = FORMAT_B && (funct3 == 3'b101) && (opcode == 7'b1100011);
+  assign BLT = FORMAT_B && (funct3 == 3'b100) && (opcode == 7'b1100011);
+  assign BLTU = FORMAT_B && (funct3 == 3'b110) && (opcode == 7'b1100011);
+  assign BEQ = FORMAT_B && (funct3 == 3'b000) && (opcode == 7'b1100011);
+  assign BNE = FORMAT_B && (funct3 == 3'b001) && (opcode == 7'b1100011);
+  assign ALLOWED_B = BGEU || BGE || BLT || BLTU || BEQ || BNE;
 
   assign FORMAT_I = (rs1 < 16) && (rd < 16);
   assign ANDI = FORMAT_I && (funct3 == 3'b111) && (opcode == 7'b0010011);
@@ -107,14 +136,23 @@ clk);
   assign ADDI = FORMAT_I && (funct3 == 3'b000) && (opcode == 7'b0010011);
   assign ALLOWED_I = ANDI || SLTIU || SRLI || SLTI || SRAI || SLLI || ORI || XORI || ADDI;
 
-  assign FORMAT_LW = (instruction[31:30] == 00) && (rs1 < 16) && (rd < 16) && (imm12 < 64);
-  assign LW = FORMAT_LW && (funct3 == 3'b010) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
-  assign ALLOWED_LW = LW;
-
-  // FIXME: rd == 0 or else counterexample - depends on PC
-  assign FORMAT_J = (rd < 16);
+  assign FORMAT_J = (rd == 0); // FIXME: rd == 0 or else counterexample - depends on PC
   assign JAL = FORMAT_J && (opcode == 7'b1101111);
   assign ALLOWED_J = JAL;
+
+  assign FORMAT_SW = (instruction[31:30] == 00) && (rs2 < 16) && (rs1 < 16) && (imm7 < 2);
+  assign SB = FORMAT_SW && (funct3 == 3'b000) && (opcode == 7'b0100011) && (rs1 == 5'b00000);
+  assign SH = FORMAT_SW && (funct3 == 3'b001) && (opcode == 7'b0100011) && (rs1 == 5'b00000);
+  assign SW = FORMAT_SW && (funct3 == 3'b010) && (opcode == 7'b0100011) && (rs1 == 5'b00000);
+  assign ALLOWED_SW = SB || SH || SW;
+
+  assign FORMAT_LW = (instruction[31:30] == 00) && (rs1 < 16) && (rd < 16) && (imm12 < 64);
+  assign LBU = FORMAT_LW && (funct3 == 3'b100) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
+  assign LH = FORMAT_LW && (funct3 == 3'b001) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
+  assign LW = FORMAT_LW && (funct3 == 3'b010) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
+  assign LHU = FORMAT_LW && (funct3 == 3'b101) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
+  assign LB = FORMAT_LW && (funct3 == 3'b000) && (opcode == 7'b0000011) && (rs1 == 5'b00000);
+  assign ALLOWED_LW = LBU || LH || LW || LHU || LB;
 
   assign FORMAT_R = (rs2 < 16) && (rs1 < 16) && (rd < 16);
   assign AND = FORMAT_R && (funct3 == 3'b111) && (opcode == 7'b0110011) && (funct7 == 7'b0000000);
@@ -129,12 +167,7 @@ clk);
   assign OR = FORMAT_R && (funct3 == 3'b110) && (opcode == 7'b0110011) && (funct7 == 7'b0000000);
   assign ALLOWED_R = AND || SLTU || SRA || XOR || SUB || SLT || SRL || SLL || ADD || OR;
 
-  assign FORMAT_SW = (instruction[31:30] == 00) && (rs2 < 16) && (rs1 < 16) && (imm7 < 2);
-  assign SW = FORMAT_SW && (funct3 == 3'b010) && (opcode == 7'b0100011) && (rs1 == 5'b00000);
-  assign ALLOWED_SW = SW;
-
-  // FIXME: rd == 0 or else counterexample - depends on PC
-  assign FORMAT_AUIPC = (rd < 16);
+  assign FORMAT_AUIPC = (rd == 0); // FIXME: rd == 0 or else counterexample - depends on PC
   assign AUIPC = FORMAT_AUIPC && (opcode == 7'b0010111);
   assign ALLOWED_AUIPC = AUIPC;
 
@@ -144,7 +177,7 @@ clk);
 
   assign NOP = (opcode == 7'b1111111);
   assign ALLOWED_NOP = NOP;
-  
+
   wire sif_commit;
   assign sif_commit = design_top.dut.sif_commit;
   // only allow certain instructions before SIF commit
@@ -153,12 +186,12 @@ clk);
   assume_allowed_instructions_before_tc: assume property (
                          @(posedge clk)
                          ~sif_commit |->
-                         (ALLOWED_I || ALLOWED_LW || ALLOWED_J || ALLOWED_LUI || ALLOWED_AUIPC || ALLOWED_R || ALLOWED_NOP)
+                         (ALLOWED_R || ALLOWED_I || ALLOWED_LW || ALLOWED_B || ALLOWED_J || ALLOWED_LUI || ALLOWED_AUIPC || ALLOWED_NOP)
                          );
   assume_allowed_instructions_after_tc: assume property (
                          @(posedge clk)
                          sif_commit |->
-                         (ALLOWED_I || ALLOWED_LW || ALLOWED_J || ALLOWED_LUI || ALLOWED_AUIPC || ALLOWED_R || ALLOWED_SW || ALLOWED_NOP)
+                         (ALLOWED_R || ALLOWED_I || ALLOWED_LW || ALLOWED_B || ALLOWED_J || ALLOWED_LUI || ALLOWED_AUIPC || ALLOWED_SW || ALLOWED_NOP)
                          );
 
 endmodule
--- steelcore_demo/qed/modify_instruction.v
+++ steelcore_demo/qed/modify_instruction.v
@@ -10,62 +10,71 @@ module modify_instruction (
 // Outputs
 qed_instruction,
 // Inputs
-shamt,
-IS_SW,
-imm12,
 IS_R,
 qic_qimux_instruction,
+jimm20,
+IS_LUI,
+IS_B,
+IS_I,
+IS_AUIPC,
+IS_J,
+rs1,
+rs2,
 rd,
 funct3,
-opcode,
-rs2,
 funct7,
-IS_I,
+IS_SW,
+imm12,
+bimm10,
+bimm11,
+bimm12,
 IS_LW,
-imm5,
-rs1,
 jimm10,
 jimm11,
-jimm19,
-jimm20,
-IS_J,
-IS_LUI,
-IS_AUIPC,
 uimm31,
-imm7);
+opcode,
+bimm4,
+imm5,
+imm7,
+jimm19);
 
-  input [4:0] shamt;
-  input IS_SW;
-  input [11:0] imm12;
   input IS_R;
   input [31:0] qic_qimux_instruction;
+  input jimm20;
+  input IS_LUI;
+  input IS_B;
+  input IS_I;
+  input IS_AUIPC;
+  input IS_J;
+  input [4:0] rs1;
+  input [4:0] rs2;
   input [4:0] rd;
   input [2:0] funct3;
-  input [6:0] opcode;
-  input [4:0] rs2;
   input [6:0] funct7;
-  input IS_I;
+  input IS_SW;
+  input [11:0] imm12;
+  input [5:0] bimm10;
+  input bimm11;
+  input bimm12;
   input IS_LW;
-  input [4:0] imm5;
-  input [4:0] rs1;
   input [9:0] jimm10;
   input jimm11;
-  input [7:0] jimm19;
-  input jimm20;
-  input IS_J;
-  input [6:0] imm7;
-  input IS_AUIPC;
   input [19:0] uimm31;
-  input IS_LUI;
+  input [6:0] opcode;
+  input [3:0] bimm4;
+  input [4:0] imm5;
+  input [6:0] imm7;
+  input [7:0] jimm19;
 
   output [31:0] qed_instruction;
 
+  wire [31:0] INS_B;
+  wire [31:0] INS_CONSTRAINT;
   wire [31:0] INS_I;
+  wire [31:0] INS_J;
+  wire [31:0] INS_SW;
   wire [31:0] INS_LW;
   wire [31:0] INS_R;
-  wire [31:0] INS_SW;
-  wire [31:0] INS_CONSTRAINT;
-  wire [31:0] INS_J;
   wire [31:0] INS_AUIPC;
   wire [31:0] INS_LUI;
 
@@ -87,14 +96,15 @@ imm7);
   // assign NEW_imm5 = {1'b1, imm5[3:0]};
   // End Edit: Shrink RAM Depth
 
+  assign INS_B = {bimm12, bimm10, NEW_rs2, NEW_rs1, funct3, bimm4, bimm11, opcode};
   assign INS_I = {imm12, NEW_rs1, funct3, NEW_rd, opcode};
+  assign INS_J = {jimm20, jimm10, jimm11, jimm19, NEW_rd, opcode};
+  assign INS_SW = {NEW_imm7, NEW_rs2, NEW_rs1, funct3, imm5, opcode};
   assign INS_LW = {NEW_imm12, NEW_rs1, funct3, NEW_rd, opcode};
   assign INS_R = {funct7, NEW_rs2, NEW_rs1, funct3, NEW_rd, opcode};
-  assign INS_SW = {NEW_imm7, NEW_rs2, NEW_rs1, funct3, imm5, opcode};
-  assign INS_J = {jimm20, jimm10, jimm11, jimm19, NEW_rd, opcode};
   assign INS_AUIPC = {uimm31, NEW_rd, opcode};
   assign INS_LUI = {uimm31, NEW_rd, opcode};
 
-  assign qed_instruction = IS_I ? INS_I : (IS_J ? INS_J : (IS_SW ? INS_SW : (IS_LW ? INS_LW : (IS_R ? INS_R : (IS_AUIPC ? INS_AUIPC : (IS_LUI ? INS_LUI : qic_qimux_instruction))))));
+  assign qed_instruction = IS_B ? INS_B : (IS_I ? INS_I : (IS_J ? INS_J : (IS_SW ? INS_SW : (IS_LW ? INS_LW : (IS_R ? INS_R : (IS_AUIPC ? INS_AUIPC : (IS_LUI ? INS_LUI : qic_qimux_instruction)))))));
 
 endmodule
