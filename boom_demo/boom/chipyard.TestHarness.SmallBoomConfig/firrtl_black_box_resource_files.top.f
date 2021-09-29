c603c9780db9c5db69e46c2bb3703c23e20146a6
Eldrick Millares <eldrick@stanford.edu>
2021-09-28 23:46:07 -0700

    Cleans up demos

--- boom_demo/boom/chipyard.TestHarness.SmallBoomConfig/firrtl_black_box_resource_files.top.f
+++
@@ -1,3 +0,0 @@
-/tmp/eldrick/chipyard/sims/vcs/generated-src/chipyard.TestHarness.SmallBoomConfig/plusarg_reader.v
-/tmp/eldrick/chipyard/sims/vcs/generated-src/chipyard.TestHarness.SmallBoomConfig/IOCell.v
-/tmp/eldrick/chipyard/sims/vcs/generated-src/chipyard.TestHarness.SmallBoomConfig/ClockDividerN.sv
--- e203_demo/e203_hbirdv2/fpga/mcu200t/script/board.tcl
+++
@@ -1,3 +0,0 @@
-set name {nuclei_mcu200t}
-set part_fpga {xc7a200tfbg484-2}
-set bootrom_inst {rom}
