45955b3450080d9f4f366d95e1d76c3e4624000d
Eldrick Millares <eldrick@stanford.edu>
2021-09-28 23:48:27 -0700

    Fixes nested directory mistake

--- demos/cva6_demo/qed/modify_instruction.v
+++ demos/cva6_demo/qed/modify_instruction.v
@@ -1,74 +1,72 @@
-// use along with input constraints to the formal tool
+// Copyright (c) Stanford University
+// 
+// This source code is patent protected and being made available under the
+// terms explained in the ../LICENSE-Academic and ../LICENSE-GOV files.
+//
+// Author: Mario J Srouji
+// Email: msrouji@stanford.edu
 
-module modify_instruction (qic_qimux_instruction,
-                           is_lw,
-                           is_sw,
-                           is_aluimm,
-			   is_alureg,
+module modify_instruction (
+// Outputs
+qed_instruction,
+// Inputs
+shamt,
+IS_SW,
+imm12,
+IS_R,
+qic_qimux_instruction,
+rd,
+funct3,
+opcode,
+rs2,
+funct7,
+IS_I,
+IS_LW,
+imm5,
+rs1,
+imm7);
 
-                           rd,
-                           rs1,
-                           rs2,
-                           simm12,
-			   opcode,
-			   funct3,
-			   funct7,
-			   imm5,
-			   shamt,
-                           simm7,
+  input [4:0] shamt;
+  input IS_SW;
+  input [11:0] imm12;
+  input IS_R;
+  input [31:0] qic_qimux_instruction;
+  input [4:0] rd;
+  input [2:0] funct3;
+  input [6:0] opcode;
+  input [4:0] rs2;
+  input [6:0] funct7;
+  input IS_I;
+  input IS_LW;
+  input [4:0] imm5;
+  input [4:0] rs1;
+  input [6:0] imm7;
 
-                           qed_instruction);
+  output [31:0] qed_instruction;
 
-   input [31:0] qic_qimux_instruction;
-   wire [31:0]  instruction;
-   assign instruction = qic_qimux_instruction;
+  wire [31:0] INS_I;
+  wire [31:0] INS_LW;
+  wire [31:0] INS_R;
+  wire [31:0] INS_SW;
+  wire [31:0] INS_CONSTRAINT;
 
-   input [4:0] 	rd;
-   input [4:0]  rs1;
-   input [4:0]  rs2;
-   input [6:0]  opcode;
-   input [11:0] simm12;  // signed imm for I type or instructions using rd, rs1 only like LW 
-   input [2:0] 	funct3;
-   input [6:0] 	funct7;
-   input [4:0] 	imm5;  // lower order imm bits for S type instruction
-   input [6:0] 	simm7; // higher order bits (including sign bits) of imm operand for S type instruction
-   input [4:0] 	shamt; // shift amount for immediate shift operations
+  wire [4:0] NEW_rd;
+  wire [4:0] NEW_rs1;
+  wire [4:0] NEW_rs2;
+  wire [11:0] NEW_imm12;
+  wire [6:0] NEW_imm7;
 
-   input 	is_lw;
-   input 	is_sw;
-   input 	is_aluimm;
-   input 	is_alureg; // includes mult instructions
+  assign NEW_rd = (rd == 5'b00000) ? rd : {1'b1, rd[3:0]};
+  assign NEW_rs1 = (rs1 == 5'b00000) ? rs1 : {1'b1, rs1[3:0]};
+  assign NEW_rs2 = (rs2 == 5'b00000) ? rs2 : {1'b1, rs2[3:0]};
+  assign NEW_imm12 = {2'b01, imm12[9:0]};
+  assign NEW_imm7 = {2'b01, imm7[4:0]};
 
-   output [31:0] qed_instruction;
-   
-   wire [31:0]   ins_lw;
-   wire [31:0]   ins_sw;
-   wire [31:0]   ins_alureg;
-   wire [31:0]   ins_aluimm;
+  assign INS_I = {imm12, NEW_rs1, funct3, NEW_rd, opcode};
+  assign INS_LW = {NEW_imm12, NEW_rs1, funct3, NEW_rd, opcode};
+  assign INS_R = {funct7, NEW_rs2, NEW_rs1, funct3, NEW_rd, opcode};
+  assign INS_SW = {NEW_imm7, NEW_rs2, NEW_rs1, funct3, imm5, opcode};
 
-   wire [4:0]    new_rd;
-   wire [4:0]    new_rs1;
-   wire [4:0]    new_rs2;
-   wire [11:0]   new_simm12;
-   wire [6:0] 	 new_simm7;
-  
-   assign new_rd  = (rd  == 5'b00000) ? rd  : {1'b1, rd[3:0]};
-   assign new_rs1 = (rs1 == 5'b00000) ? rs1 : {1'b1, rs1[3:0]};
-   assign new_rs2 = (rs2 == 5'b00000) ? rs2 : {1'b1, rs2[3:0]};
-   assign new_simm12 = {7'b0000001, simm12[6:0]};  // changes for duplicate LW instruction
-   //assign new_simm7 =  {5'b00001, simm7[1:0]};  // changes for duplicate SW instruction
-   assign new_simm7 =   7'b0000001;  // changes for duplicate SW instruction
-   
-   assign ins_lw    = {new_simm12, 5'b0, funct3, new_rd, opcode};
-   assign ins_sw    = {new_simm7, 5'b0, new_rs1, funct3, imm5, opcode};
-   assign ins_aluimm = {simm12, new_rs1, funct3, new_rd, opcode};
-   assign ins_alureg = {funct7, new_rs2, new_rs1, funct3, new_rd, opcode};
-      
+  assign qed_instruction = IS_I ? INS_I : (IS_LW ? INS_LW : (IS_R ? INS_R : (IS_SW ? INS_SW : qic_qimux_instruction)));
 
-   assign qed_instruction = is_lw ? ins_lw : (is_sw ? ins_sw : (is_alureg ? ins_alureg : 
-								(is_aluimm ? ins_aluimm : instruction)));
-   
-   
-endmodule // modify_instruction
-
-                           
+endmodule
